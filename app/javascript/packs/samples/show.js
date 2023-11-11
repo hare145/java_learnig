3adc993b997dbe964dafd75593776e9c5068a5ec
hare145 <130465393+hare145@users.noreply.github.com>
2023-11-11 10:36:21 +0000

    clic

--- app/javascript/packs/samples/show.js
+++ app/javascript/packs/samples/show.js
@@ -7,3 +7,12 @@ $(function(){
     $('.boxred').css({'background-color': '#0000FF'});
   });
 });
+
+$(function(){
+  $('.boxred').on('click', function(){
+    $('.boxred').addClass('box1-ext');
+  });
+  $('.boxred').mouseout(function(){
+    $('.boxred').removeClass('box1-ext');
+  });
+});
