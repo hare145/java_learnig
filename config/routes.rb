c7bf1e1d6136645cb4c3c1fb75444f96cc0efb58
hare145 <130465393+hare145@users.noreply.github.com>
2023-11-12 15:53:45 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   
-  
-  get 'backs/index'
-  get 'backs/show'
   controller :homes do
     root to: "homes#top"
     resources :homes, only: [:index]
   end
+  
+  controller :samples do
+    resources :samples, only: [:index, :show]
+  end
+  
+  controller :backs do
+    resources :backs, only: [:index, :show]
+  end
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
